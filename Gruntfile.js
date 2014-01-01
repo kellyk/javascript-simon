90f31c004a22d08d8f0ffe7eb3fca64b8c981be1
Kelly King <kelly.danielle.king@gmail.com>
2014-01-01 16:00:35 -0500

    add jshint and fix timing events

---
+++ Gruntfile.js
@@ -0,0 +1,79 @@
+module.exports = function(grunt) {
+    'use strict';
+
+    var file_list =  [
+        'js/simon.js'
+    ];
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        jshint: {
+            options: {
+                // Enforcing options
+                curly: true
+                // ,forin: true
+                ,eqeqeq: true
+                ,immed: true
+                ,latedef: 'nofunc'
+                ,newcap: true
+                ,noarg: true
+                ,noempty: true
+                ,quotmark: 'single'
+                // ,undef: true
+                ,unused: true
+                ,strict: true
+                ,trailing: true
+                ,browser: true
+                ,globals: {
+                    node: true
+                    ,jQuery: true
+                    ,global: true
+                    ,Meteor: true
+                    ,Accounts: true
+                    ,_: true
+                    ,Ideas: true
+                    ,RouteController: true
+                    ,HomeController: true
+                    ,using: true
+                    ,define: true
+                    ,Notifications: true
+                    ,Session: true
+                    ,Router: true
+                    ,Template: true
+                    ,process: true
+                }
+
+                // Relaxing options
+                ,boss: true
+                ,laxcomma: true
+                ,globalstrict: true
+            },
+            uses_defaults: file_list.concat(['Gruntfile.js'])
+        },
+
+        watch: {
+            scripts: {
+                files: file_list,
+                tasks: ['jshint'],
+                options: {
+                    spawn: false
+                    ,interrupt: true
+                },
+            },
+        },
+    });
+
+    // Load tasks
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task(s).
+    grunt.registerTask('default', ['jshint']);
+
+    // On file change only run jshint on the changed file not the whole thing
+    grunt.event.on('watch', function(action, filepath) {
+        grunt.config('jshint.uses_defaults', filepath);
+    });
+};
--- js/simon.js
+++ js/simon.js
@@ -1,15 +1,15 @@
-var Simon = (function() {
-    "use strict";
- 
+(function() {
+	'use strict';
+	
 	var sequence, copy, round;
 	var active = true;
-	var mode = "normal";
+	var mode = 'normal';
 
 	$(document).ready(function() {
 		initSimon();
-    });
-   
-    function initSimon() {
+	});
+	
+	function initSimon() {
 		$('[data-action=start]').on('click', startGame);
 		$('input[name=mode]').on('change', changeMode);
 	}
@@ -20,22 +20,16 @@ var Simon = (function() {
 		round = 0;
 		$('p[data-action="lose"]').hide();
 		newRound();
-    }
+	}
 
-    function newRound() {
+	function newRound() {
 		$('[data-round]').text(++round);
 		sequence.push(randomNumber());
 		copy = sequence.slice(0);
 		animate();
+	}
 
-		// want this executed only after animate function completed
-		// would prefer more graceful solution than this eventually:
-		setTimeout(function() {
-			activateSimonBoard();
-		}, 700 * (sequence.length - 1));
-    }
-
-    function activateSimonBoard(){
+	function activateSimonBoard(){
 		$('.simon')
 			.on('click', '[data-tile]', registerClick)
 
@@ -47,33 +41,46 @@ var Simon = (function() {
 			.on('mouseup', '[data-tile]', function(){
 				$(this).removeClass('active');
 			});
-    }
+		$('[data-tile]').addClass('hoverable');
+	}
 
-    function registerClick(e) {
-		var desiredResponse = copy.shift();
-		var actualResponse = $(e.target).data('tile');
-		active = (desiredResponse === actualResponse);
-		checkLose();
-    }
-
-    function deactivateSimonBoard() {
-		if (mode !== "free-board") {
+	function deactivateSimonBoard() {
+		if (mode !== 'free-board') {
 			$('.simon')
 				.off('click', '[data-tile]')
 				.off('mousedown', '[data-tile]')
 				.off('mouseup', '[data-tile]');
+
+			$('[data-tile]').removeClass('hoverable');
 		}
-    }
+	}
+
+	function animate() {
+		var i = 0;
+		var interval = setInterval(function() {
+			playSound(sequence[i]);
+			lightUp(sequence[i]);
+
+            i++;
+            if (i >= sequence.length) {
+				clearInterval(interval);
+				activateSimonBoard();
+            }
+       }, 700);
+	}
+
+	function registerClick(e) {
+		var desiredResponse = copy.shift();
+		var actualResponse = $(e.target).data('tile');
+		active = (desiredResponse === actualResponse);
+		checkLose();
+	}
 
 	function checkLose() {
 		// copy array will be empty when user has successfully completed sequence
 		if (copy.length === 0 && active) {
 			deactivateSimonBoard();
-
-			// slight pause between rounds
-			setTimeout(function() {
-				newRound();
-			}, 1000);
+			newRound();
 
 		} else if (!active) { // user lost
 			deactivateSimonBoard();
@@ -81,29 +88,20 @@ var Simon = (function() {
 		}
 	}
 
-    function endGame() {
+	function endGame() {
 		// notify the user that they lost
 		$('p[data-action=lose]').show();
-		$($('[data-round]')[0]).text("0");
-    }
-
-    function changeMode(e) {
-		mode = e.target.value;
-    }
-
-    /*----------------- Helper functions -------------------*/
-
-    function animate() {
-		$.each(sequence, function(key, tile) {
-			setTimeout(function() {
-				playSound(tile);
-				lightUp(tile);
-			}, 700 * key);
-		});
+		$($('[data-round]')[0]).text('0');
 	}
 
-    function lightUp(tile) {
-		if (mode !== "sound-only") {
+	function changeMode(e) {
+		mode = e.target.value;
+	}
+
+	/*----------------- Helper functions -------------------*/
+
+	function lightUp(tile) {
+		if (mode !== 'sound-only') {
 			$('[data-tile=' + tile + ']').animate({
 				opacity: 1
 			}, 300, function() {
@@ -112,16 +110,16 @@ var Simon = (function() {
 				}, 300);
 			});
 		}
-    }
+	}
 
-    function playSound(tile) {
-		if (mode !== "light-only") {
+	function playSound(tile) {
+		if (mode !== 'light-only') {
 			var audio = $('<audio autoplay></audio>');
 			audio.append('<source src="sounds/' + tile + '.ogg" type="audio/ogg" />');
 			audio.append('<source src="sounds/' + tile + '.mp3" type="audio/mp3" />');
 			$('[data-action=sound]').html(audio);
 		}
-    }
+	}
 
 	function randomNumber() {
 		// between 1 and 4
